0b2bbb574477b133b3d290d1a8cdd53893cf1b6e
jose.guilherme <jose.guilherme@ikatec.com.br>
2023-03-04 01:15:13 -0300

    ajustes no layout

--- scripts/script.js
+++ scripts/script.js
@@ -140,17 +140,12 @@ handleChange = () => {
     var teste4 = document.getElementById('textLabel')
     if(changeState() === true) {
         teste2.classList.toggle('pianoKids')
-        teste3.style.backgroundColor = '#48829d'
+        teste3.style.backgroundColor = '#48829da9'
         teste3.style.transition = '1s'
-        teste4.style.color = '#000000'
-        teste4.style.textShadow = '0px 0px 3px #111111'
     } else {
         teste2.classList.toggle('pianoKids')
         teste3.style.backgroundColor = '#dddddd'
         teste3.style.transition = '1s'
-        teste4.style.color = '#ffffff'
-        teste4.style.textShadow = '0px 0px 3px #dddddd'
-
     }
 }
 
